04ad2bbcd4273c92667632d168fea1d23784740f
masuguri <kikiosn@naver.com>
2020-11-26 15:38:59 +0900

    비밀정보보호

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     name: {
         type: String,
-        maxlength; 50
+        maxlength: 50
     },
     email: {
         type: String,
